6825ca5cb636ae6f896dae567e106dcdc7d0560b
Yuheng Cui <ycui0008@student.monash.edu>
2021-09-29 17:24:07 +1000

    create documentation for who_covid data set

---
+++ R/who_covid.R
@@ -0,0 +1,23 @@
+#' Global covid-19 cases and deaths
+#'
+#' @description
+#' This dataset contains covid-19 cases and deaths of 238 countries. The date of the data set is 2021-09-28, originally.
+#'
+#' @format  A data frame with columns:
+#' \describe{
+#'  \item{name}{Full country names.}
+#'  \item{who_region}{Region defined by WHO.}
+#'  \item{cases_cumulative_total}{Total number of covid-19 cases.}
+#'  \item{cases_cumulative_total_per_100000_population}{Number of covid-19 cases in every 100000 people.}
+#'  \item{deaths_cumulative_total}{Total number of covid-19 deaths.}
+#'  \item{deaths_cumulative_total_per_100000_population}{Number of deaths due to covid-19 in every 100000 people.}
+#' }
+#'
+#'
+#'
+#' @source
+#' World Health Organisation (WHO)
+#' <https://covid19.who.int/WHO-COVID-19-global-table-data.csv>, downloaded 2021-09-28
+#'
+#'
+"who_covid"
